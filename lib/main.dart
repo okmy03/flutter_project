ce97b9d4534971374b010156adc5277fcc93abb3
okmy03 <40584908+okmy03@users.noreply.github.com>
2021-07-19 20:11:37 +0900

    testweb

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 // Uncomment lines 7 and 10 to view the visual layout at runtime.
 // import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
-void main() {
+void main() {      
   // debugPaintSizeEnabled = true;
   runApp(MyApp());
 }
